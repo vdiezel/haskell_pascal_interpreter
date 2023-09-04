0cc4eca1641261e4060aa63836b57bd6737b203c
Viktor Diezel <viktor.diezel@posteo.de>
2023-09-04 16:41:02 +0200

    small cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,6 @@
 
 module Main where
 
-import Control.Monad.State
-import Control.Applicative
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import Text.Regex.TDFA
-import Data.Char
-import qualified Data.Sequence as Seq
-import Debug.Trace
-import Data.Maybe
 import Lexer
 
 main :: IO ()
--- programs/program1.pas
+++ programs/program1.pas
@@ -8,7 +8,7 @@ BEGIN {Part10}
    BEGIN
       number := 2;
       a := number;
-      b := 10 * a + 10 * number DIV 4;
+      b := 10c * a + 10 * number DIV 4;
       c := a - - b
    END;
    x := 11;
